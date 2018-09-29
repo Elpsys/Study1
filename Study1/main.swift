83dba5197ba60e0eff85e3c7718aa9b97779f1c7
s20171103186 <s20171103186@admindeiMac-34.local>
2018-09-29 09:24:00 +0800

    排序
    从大到小排序

--- Study1/main.swift
+++ Study1/main.swift
@@ -8,5 +8,30 @@
 
 import Foundation
 
-print("Hello, World!")
+import Cocoa
+
+var all = [10,1,2,3,5,4,9,6,7,8]
+var n = 0
+var i = 0
+var m = 0
+var temp = 0
+for n in 0 ..< 10
+{
+    
+    for i in 0 ..< 10
+    {
+        if all[n] > all[i]
+        {
+            temp = all[n]
+            all[n] = all[i]
+            all[i] = temp
+        }
+    }
+}
+while m != 10
+{
+    print(all[m])
+    m = m + 1
+}
+
 
